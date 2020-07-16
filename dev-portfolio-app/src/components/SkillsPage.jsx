e7b11dc864c1109a0233ebb9c56e53e4e330521e
Brandon Davis <bdavis171@gmail.com>
2020-07-16 07:29:58 -0500

    fixing images for skills page

--- dev-portfolio-app/src/components/SkillsPage.jsx
+++ dev-portfolio-app/src/components/SkillsPage.jsx
@@ -14,15 +14,15 @@ class SkillsPage extends Component {
                 
                 <div id="skills-img">
                     <a href="https://en.wikipedia.org/wiki/HTML5" target='_blank'>
-                    <img src="https://www3.lunapic.com/do-not-link-here-use-hosting-instead/158583443050042189?6144725562" alt="html5" id='html5'/>
+                    <img src="/html-logo.png" alt="html5" id='html5'/>
                     </a>
 
                     <a href="https://en.wikipedia.org/wiki/Cascading_Style_Sheets" target='_blank'>
-                    <img src="https://www3.lunapic.com/do-not-link-here-use-hosting-instead/158583443050042189?7160127127" alt="css3" id='css3'/>
+                    <img src="/css-logo.png" alt="css3" id='css3'/>
                     </a>
 
                     <a href="https://en.wikipedia.org/wiki/JavaScript" target='_blank'>
-                    <img src="https://www3.lunapic.com/do-not-link-here-use-hosting-instead/158583443050042189?6464397002" alt="javascript" id='javascript'/>
+                    <img src="/javascript-logo.jpg" alt="javascript" id='javascript'/>
                     </a>
 
                     <a href="https://en.wikipedia.org/wiki/React_(web_framework)" target='_blank'>
